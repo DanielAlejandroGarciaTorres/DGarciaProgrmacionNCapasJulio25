5362b5282a82f69ba97f8ce5d07580c8a08cb731
digis <digis@DESKTOP-Q0UH1ID>
2025-08-14 11:27:32 -0600

    Flujo completo

--- src/main/java/com/digis01/KMedranoProgramacionNCapasJulio25/Controller/AlumnoController.java
+++ src/main/java/com/digis01/KMedranoProgramacionNCapasJulio25/Controller/AlumnoController.java
@@ -24,16 +24,16 @@ public class AlumnoController {
 
     @Autowired
     private AlumnoDAOImplementation alumnoDAOImplementation;
-    
+
     @Autowired
     private EstadoDAOImplementation estadoDAOImplementation;
-    
+
     @Autowired
     private MunicipioDAOImplementation municipioDAOImplementation;
 
     @Autowired
     private SemestreDAOImplementatiton semestreDAOImplementatiton;
-    
+
     @GetMapping // verbo http GET, POST, PUT, DELETE, PATCH
     public String Index(Model model) {
         Result result = alumnoDAOImplementation.GetAll();
@@ -47,28 +47,27 @@ public class AlumnoController {
         return "AlumnoIndex";
     }
 
-    @GetMapping("alumnoDetail/{idAlumno}")
-    public String AlumnoDetail(@PathVariable int idAlumno, Model model) {
+    @GetMapping("/action/{IdAlumno}") // localhost:8081/alumno/add
+    public String add(Model model, @PathVariable("IdAlumno") int IdAlumno) {
+//        Result result = semestreDAOImplementatiton.GetAll();
 
-        Result result = alumnoDAOImplementation.DireccionesByIdAlumno(idAlumno);
+        if (IdAlumno == 0) {
+            model.addAttribute("semestres", semestreDAOImplementatiton.GetAll().objects);
+            model.addAttribute("Alumno", new Alumno());
 
-        if (result.correct) {
-            model.addAttribute("alumno", result.object);
+            return "AlumnoForm";
         } else {
-            return "Error";
+            Result result = alumnoDAOImplementation.DireccionesByIdAlumno(IdAlumno);
+
+            if (result.correct) {
+                model.addAttribute("alumno", result.object);
+            } else {
+                return "Error";
+            }
+
+            return "AlumnoDetail";
         }
 
-        return "AlumnoDetail";
-    }
-
-    @GetMapping("add") // localhost:8081/alumno/add
-    public String add(Model model) {
-//        Result result = semestreDAOImplementatiton.GetAll();
-        
-        model.addAttribute("semestres", semestreDAOImplementatiton.GetAll().objects);
-        model.addAttribute("Alumno", new Alumno());
-
-        return "AlumnoForm";
     }
 
     //Proceso de agregado
@@ -85,13 +84,13 @@ public class AlumnoController {
             return "redirect:/alumno";
         }
     }
-    
+
     //getMunicipioByEstado?IdEstado=7 -- requestParam
     //getMunicipioByestado/7 -- pathVariable
     @GetMapping("getMunicipiosByIdEstado/{IdEstado}")
     @ResponseBody // retorne un dato estructurado - JSON
-    public Result MunicipioByEstado(@PathVariable int IdEstado){
-      
+    public Result MunicipioByEstado(@PathVariable int IdEstado) {
+
         return municipioDAOImplementation.MunicipioByEstado(IdEstado);
     }
 }
